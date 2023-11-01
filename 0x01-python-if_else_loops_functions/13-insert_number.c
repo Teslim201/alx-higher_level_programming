8b44b5cc594246c78fac60aee818d439030f466d
teslim201 <teslim201@gmail.com>
2023-10-31 18:40:30 -0700

    phyton

--- 0x01-python-if_else_loops_functions/13-insert_number.c
+++ 0x01-python-if_else_loops_functions/13-insert_number.c
@@ -3,12 +3,12 @@
 listint_t *create_node(int n);
 
 /**
- * insert_node - this is what inserts a node sorted in a linked list of ints
- * @head:  this is the only double pointer to head of LL, needed for
- * modification in edge cases
- * @number: this is the new data for new node
+ * insert_node - this only inserts a node sorted in a linked list of ints
+ * @head: the only double pointer to head of LL, needed for modification 
+ * in edge cases
+ * @number: the only data for new node
  *
- * Return: this is the only pointer to newly created node, NULL on failure
+ * Return: the only pointer to newly created node, NULL on failure
  */
 listint_t *insert_node(listint_t **head, int number)
 {
@@ -25,7 +25,7 @@ listint_t *insert_node(listint_t **head, int number)
 	cur_node = *head;
 	while (cur_node)
 	{
-		/* thiis needs to insert a at head */
+		/* this only need to insert at head */
 		if (cur_node->n >= number)
 		{
 			new_node = create_node(number);
@@ -33,7 +33,9 @@ listint_t *insert_node(listint_t **head, int number)
 			*head = new_node;
 			return (new_node);
 		}
-		else if (cur_node->n next || cur_node->next->n >= number)
+		else if (cur_node->n <= number)
+		{
+			if (!cur_node->next || cur_node->next->n >= number)
 			{
 				new_node = create_node(number);
 				new_node->next = cur_node->next;
@@ -43,15 +45,15 @@ listint_t *insert_node(listint_t **head, int number)
 		}
 		cur_node = cur_node->next;
 	}
-	return (NULL); /* failed */
+	return (NULL); /*if failed */
 }
 
 
 /**
- * create_node - this is what creates a new node for the LL
- * @n: this is the only data to insert into new node
+ * create_node - this only creates a new node for the LL
+ * @n: the only data to insert into new node
  *
- * Return: this is what point to newly allocated node
+ * Return: the only pointer to newly allocated node
  */
 listint_t *create_node(int n)
 {
